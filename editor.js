418bc68dad5d3a7ebb584209a01721bd171c71c0
Kenyg369 <kengrand369@gmail.com>
2024-03-29 01:30:40 +0800

    Refactor css

--- editor.js
+++ editor.js
@@ -6,60 +6,58 @@ export function setupEditor(element) {
 		sectionToHide.classList.toggle("hidden");
 		document.querySelector("#app").innerHTML = `
             <div class="editor">			
-                <nav> 
-                    <ul class="control-buttons">
-                        <li><button class="control-button"><i class="fa-solid fa-arrow-up-from-bracket icon"></i>Upload</button></li>
-                        <li><button class="control-button"><i class="fa-solid fa-download icon"></i>Save</button></li>
-                        <li><button class="control-button"><i class="fa-brands fa-audible icon"></i>Audit</button></li>
-                        <li><button class="control-button"><i class="fa-solid fa-xmark icon"></i>Close</button></li>
+                <nav class="editor__nav"> 
+                    <ul class="editor__control-buttons">
+                        <li><button class="editor__control-button"><i class="fa-solid fa-arrow-up-from-bracket editor__icon"></i>Upload</button></li>
+                        <li><button class="editor__control-button"><i class="fa-solid fa-download editor__icon"></i>Save</button></li>
+                        <li><button class="editor__control-button"><i class="fa-brands fa-audible editor__icon"></i>Audit</button></li>
+                        <li><button class="editor__control-button"><i class="fa-solid fa-xmark editor__icon"></i>Close</button></li>
                     </ul>	
                 </nav>
-                <div class="editor-container">
-                    <div class="editor-header">
-                        <span class="ids-info">IDS File: xxxxxxxxxxxxxxx.ids</span>
+                <div class="editor__container">
+                    <div class="editor__header">
+                        <span class="editor__ids-info">IDS File: xxxxxxxxxxxxxxx.ids</span>
                     </div>
-                    <div class="metadata">
-                      <div class="metadata-item">
-                        <i class="fas fa-envelope icon icon--metadata"></i>
-                        <span class="metadata-value">example@gmail.com</span>
-                      </div>
-                      <div class="metadata-item">
-                        <i class="fas fa-calendar-alt icon icon--metadata"></i>
-                        <span class="metadata-value">April 1, 2024</span>
-                      </div>
-                      <div class="metadata-item">
-                        <i class="fas fa-flag icon icon--metadata"></i>
-                        <span class="metadata-value">Construction</span>
-                      </div>
-                      <div class="metadata-item">
-                        <i class="fas fa-code-branch icon icon--metadata"></i>
-                        <span class="metadata-value">Ver.1.0</span>
-                      </div>
+                    <div class="editor__metadata">
+                        <div class="editor__metadata-item">
+                            <i class="fas fa-envelope editor__icon editor__icon--metadata"></i>
+                            <span class="editor__metadata-value">example@gmail.com</span>
+                        </div>
+                        <div class="editor__metadata-item">
+                            <i class="fas fa-calendar-alt editor__icon editor__icon--metadata"></i>
+                            <span class="editor__metadata-value">April 1, 2024</span>
+                        </div>
+                        <div class="editor__metadata-item">
+                            <i class="fas fa-flag editor__icon editor__icon--metadata"></i>
+                            <span class="editor__metadata-value">Construction</span>
+                        </div>
+                        <div class="editor__metadata-item">
+                            <i class="fas fa-code-branch editor__icon editor__icon--metadata"></i>
+                            <span class="editor__metadata-value">Ver.1.0</span>
+                        </div>
                     </div>
-                    <div class="editor-body">
-                      <div class="editor-gutter">
-                        <button class="editor-button add-section"><i class="fa-solid fa-plus fa-flip-horizontal fa-2xs"></i></button>
-                        <button class="editor-button remove-section"><i class="fa-solid fa-trash fa-2xs"></i></button>
-                      </div>
-                      <div>
-                        <div class="sections">
+                    <div class="editor__body">
+                        <div class="editor__gutter">
+                            <button class="editor__button add-section"><i class="fa-solid fa-plus fa-flip-horizontal fa-xs"></i></button>
+                            <button class="editor__button remove-section"><i class="fa-solid fa-trash fa-xs"></i></button>
+                        </div>
+                        <div class="content">
                             <div class="section">
                                 <h2 class="">Requirement name:</h2>
                                 <div class="spec-contents">
-                                    <p class="editable editable--description" contenteditable>Describe why the requirement is important to the project.</p>
-                                    <p class="editable editable--description" contenteditable>Provide instructions on who is responsible and how to achieve it.</p>
+                                    <p class="section__editable description" contenteditable>Describe why the requirement is important to the project.</p>
+                                    <p class="section__editable description" contenteditable>Provide instructions on who is responsible and how to achieve it.</p>
                                 </div>
                                 <h3>APPLIES TO:</h3>
                                 <div class="applicables">
-                                    <span class="editable editable--facet" contenteditable>xxxxxxxxxxxxx</span>
+                                    <span class="section__editable facet" contenteditable>xxxxxxxxxxxxx</span>
                                 </div>
                                 <h3>REQUIREMENTS:</h3>
                                 <div class="requirements">
-                                    <span class="editable editable--facet" contenteditable>yyyyyyyyyyyyy</span>
+                                    <span class="section__editable facet" contenteditable>yyyyyyyyyyyyy</span>
                                 </div>
                             </div>
                         </div>
-                      </div>
                     </div>
                   </div>
             </div>
@@ -71,6 +69,7 @@ export function setupEditor(element) {
 	function setUpEditorSideBar() {
 		// Function to update editor body height
 		function updateEditorHeight() {
+			const editorBody = document.querySelector(".editor");
 			const sections = document.querySelectorAll(".section");
 			let totalHeight = 0;
 			sections.forEach((section) => {
@@ -82,26 +81,26 @@ export function setupEditor(element) {
 
 		const addSectionBtn = document.querySelector(".add-section");
 		const removeSectionBtn = document.querySelector(".remove-section");
-		const editorContent = document.querySelector(".sections");
+		const editorContent = document.querySelector(".content");
 
 		// Function to add a new requirement
 		function addSection() {
 			const newSection = document.createElement("div");
 			newSection.classList.add("section");
 			newSection.innerHTML = `
-                <div class="section-separator"></div>
+                <div class="section__separator"></div>
                 <h2 class="">Requirement name:</h2>
                 <div class="spec-contents">
-                    <p class="editable editable--description" contenteditable>Describe why the requirement is important to the project.</p>
-                    <p class="editable editable--description" contenteditable>Provide instructions on who is responsible and how to achieve it.</p>
+                    <p class="section__editable description" contenteditable>Describe why the requirement is important to the project.</p>
+                    <p class="section__editable description" contenteditable>Provide instructions on who is responsible and how to achieve it.</p>
                 </div>
                 <h3>APPLIES TO:</h3>
                 <div class="applicables">
-                    <span class="editable editable--facet" contenteditable>xxxxxxxxxxxxx</span>
+                    <span class="section__editable facet" contenteditable>xxxxxxxxxxxxx</span>
                 </div>
                 <h3>REQUIREMENTS:</h3>
                 <div class="requirements">
-                    <span class="editable editable--facet" contenteditable>yyyyyyyyyyyyy</span>
+                    <span class="section__editable facet" contenteditable>yyyyyyyyyyyyy</span>
                 </div>
             `;
 			editorContent.appendChild(newSection);
--- main.js
+++ main.js
@@ -7,19 +7,19 @@ import { setupEditor } from "./editor.js";
 document.querySelector("#entry").innerHTML = `
     <div>
         <a href="https://osarch.org/" target="_blank">
-            <img src="${OSArchLogo}" class="logo" alt="OSArch Logo" />
+            <img src="${OSArchLogo}" class="entry__logo" alt="OSArch Logo" />
         </a>
         <a href="https://www.blender.org/features/" target="_blank">
-            <img src="${blenderLogo}" class="logo vanilla" alt="Blender Logo" />
+            <img src="${blenderLogo}" class="entry__logo entry__logo--vanilla" alt="Blender Logo" />
         </a>
         <a href="https://blenderbim.org/" target="_blank">
-        <img src="${blenderBIMLogo}" class="logo vanilla" alt="BlenderBIM Logo" />
+        <img src="${blenderBIMLogo}" class="entry__logo entry__logo--vanilla" alt="BlenderBIM Logo" />
         </a>
         <h1>Hello IFCTester!</h1>
-        <div class="card">
+        <div class="entry__card">
             <button id="start-button">Let's start!</button>
         </div>
-        <p class="read-the-docs">
+        <p class="entry__read-the-docs">
             Click to start
         </p>
     </div>
